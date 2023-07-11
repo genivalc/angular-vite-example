9cd86134180f89958c1be281d48c676a7864beab
genivalc <genivalcandeiadon.neto@gmail.com>
2023-07-11 17:41:52 -0300

    dinamic component

--- src/comoponets/about/about.component.ts
+++ src/comoponets/about/about.component.ts
@@ -6,8 +6,30 @@ import { CommonModule } from '@angular/common';
     standalone: true,
     imports: [CommonModule],
     template: `
-        <p> hello, this is about us page </p>
+        <div *ngFor="let item of items">
+            <h2>{{ item.title }}</h2>
+            <p>{{ item.description }}</p>
+            <img [src]="item.image" alt="{{ item.title }}">
+        </div>
     `,
 })
 
-export class AboutComponent {}
+export class AboutComponent {
+    items = [
+        {
+          title: 'Item 1',
+          description: 'This is the description for item 1',
+          image: 'https://example.com/item1.jpg'
+        },
+        {
+          title: 'Item 2',
+          description: 'This is the description for item 2',
+          image: 'https://example.com/item2.jpg'
+        },
+        {
+          title: 'Item 3',
+          description: 'This is the description for item 3',
+          image: 'https://example.com/item3.jpg'
+        }
+      ];
+}
--- src/comoponets/home/home.component.ts
+++ src/comoponets/home/home.component.ts
@@ -6,8 +6,41 @@ import { CommonModule } from '@angular/common';
     standalone: true,
     imports: [CommonModule],
     template: `
-        <p> hello, this is home page </p>
-    `,
+         <div class="dynamic-template" [ngClass]="{'red': isRed, 'blue': isBlue}">
+      <h1>{{ title }}</h1>
+      <p>{{ content }}</p>
+      <button (click)="changeColor()">{{ titleButton }}</button>
+    </div>
+  `,
+  styles: [
+    `
+     .dynamic-template {
+        background-color: #fff;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+      }
+     .red {
+        background-color: #ff0000;
+      }
+     .blue {
+        background-color: #0000ff;
+      }
+    `
+  ],
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    isRed = true;
+    isBlue = false;
+    title = 'Dynamic Template';
+    content = 'This is a dynamic template created in Angular using CSS.';
+    titleButton = 'BLUE'
+
+    changeColor() {
+        this.isBlue = !this.isBlue
+        this.isRed = !this.isRed
+        this.isBlue ? this.titleButton = "RED" : this.titleButton = "BLUE"
+    }
+
+}
